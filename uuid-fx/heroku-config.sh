49008340d894428699c3db39605673b29f736cbb
Chris <cwall@salesforce.com>
2018-09-04 11:21:40 -0500

    More monorepo re-org

--- uuid-fx/heroku-config.sh
+++ uuid-fx/heroku-config.sh
@@ -5,9 +5,10 @@ heroku config:set \
   APP_BASE=uuid-fx \
   SALESFORCE_ACCESS_TOKEN=00DL00000061k1z!ARUAQPKCPuTgWFcM_Um_g4LVp.ijzWmSMQpynUdFQTI0PhkxyBLhUaylgIx02RbrZA7pOJTG5c2hvZxXSS.gCsjs3U2tpE39 \
   SALESFORCE_INSTANCE_URL=https://app-speed-745-dev-ed.cs8.my.salesforce.com \
-  SALESFORCE_USERNAME=test-j7blajfbxxx5@example.com \
-  SALESFORCE_PASSWORD=eventdriven1 \
   VERBOSE=true \
   PLUGIN_NAMES=invoke-functions \
   OBSERVE_SALESFORCE_TOPIC_NAMES=/event/Heroku_Function_Generate_UUID_Invoke__e \
   READ_MODE=changes
+
+#SALESFORCE_USERNAME=test-j7blajfbxxx5@example.com \
+#SALESFORCE_PASSWORD=eventdriven1 \
--- uuid-fx/run.sh
+++ uuid-fx/run.sh
@@ -1,3 +1,5 @@
+heroku config:set SALESFORCE_ACCESS_TOKEN=null
+
 READ_MODE=changes \
 PLUGIN_NAMES=invoke-functions \
 OBSERVE_SALESFORCE_TOPIC_NAMES=/event/Heroku_Function_Generate_UUID_Invoke__e \
